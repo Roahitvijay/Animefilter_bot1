3be7f95de0d35c0aab2e2d2cd8cd8fb46eb32aa9
Roahitvijay <100504788+Roahitvijay@users.noreply.github.com>
2022-03-06 09:46:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,4 +1,4 @@
-class Animefilter_bot(object):
+class Animefilter_bot1(object):
 
     DEFAULT_MSG = """👋Hello {mention}.....!!!\nIt's Power Full [{bot_name}](t.me/{bot_username}) Here 😎\nAdd Me To Your Group And Make Sure I'm an Admin There! \nAnd Enjoy My Pever Show.....!!!🤪"""
 
