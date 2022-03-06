32e6eec3724f02877cb7d0749f19e6e9640bdedc
Roahitvijay <100504788+Roahitvijay@users.noreply.github.com>
2022-03-06 09:45:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/Roahitvijay/Animefilter_bot1.git
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Animefilter_bot
+  git clone $UPSTREAM_REPO /Animefilter_bot1
 fi
-cd /Animefilter_bot
+cd /Animefilter_bot1
 pip3 install -U -r requirements.txt
-echo "Starting Animefilter_bot...."
+echo "Starting Animefilter_bot1...."
 python3 main.py
