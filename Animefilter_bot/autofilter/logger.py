aa73ddd4574d3fb9a5f4d323042235c6b5f3e0a4
Roahitvijay <100504788+Roahitvijay@users.noreply.github.com>
2022-03-06 09:19:45 +0530

    Update logger.py

--- Animefilter_bot/autofilter/logger.py
+++ Animefilter_bot/autofilter/logger.py
@@ -1,10 +1,10 @@
 import logging
-from pyrogram import Client as Animefilter_bot, filters as Worker
+from pyrogram import Client as Animefilter_bot1, filters as Worker
 from config import ADMINS
 logger = logging.getLogger(__name__)
 
 
-@Animefilter_bot.on_message(Worker.command('logger') & Worker.user(ADMINS))
+@Animefilter_bot1.on_message(Worker.command('logger') & Worker.user(ADMINS))
 async def log_file(bot, message):
     """Send log file"""
     try:
